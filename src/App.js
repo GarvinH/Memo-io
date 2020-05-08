f2fa9318a1de8bf818ec63799f7982b34ea2a3b4
GarvinH <garvinh2001@hotmail.com>
2020-05-08 17:07:37 -0400

    Remove testing output. Fix again note deletion order.

--- src/App.js
+++ src/App.js
@@ -12,39 +12,40 @@ class App extends Component {
 
   addNewNote = () => {
     let notes = [...this.state.notes];
-    notes.push({color: "yellow", text:"", iden: this.state.currentID});
-    this.setState({ notes: notes });
+    notes.push({ color: "yellow", text: "", iden: this.state.currentID });
+    this.setState((prevState) => { 
+      return { notes: notes,
+      currentID: prevState.currentID+1 } });
   }
 
   deleteNote = (index) => {
     let notes = [...this.state.notes];
     notes.splice(index, 1);
-    this.setState({notes: notes});
+    this.setState({ notes: notes });
   }
 
   resizeNote = (event, index) => {
     let img = new Image()
-    img.src=" "
+    img.src = " "
     event.dataTransfer.setDragImage(img, 0, 0);
     const mouseX = event.pageX;
-    const mouseY = event.pageY-66;
+    const mouseY = event.pageY - 66;
     console.log(mouseX + " " + mouseY)
     //console.log("offset: " + event.target.offsetTop + " " + event.target.offsetLeft)
     let notes = [...this.state.notes];
     notes[index].width = (notes[index].width > 99) ? (mouseX - notes[index].left) : 100;
     notes[index].height = (notes[index].height > 99) ? (mouseY - notes[index].top) : 100;
     //console.log(notes[index].width + " " + notes[index].height)
-    this.setState({notes: notes});
+    this.setState({ notes: notes });
   }
 
   render() {
-    console.log("app render")
     return (
       <ButtonContext.Provider value={{
         addNote: this.addNewNote,
       }}>
         <Layout>
-          <BulletinBoard notes={this.state.notes} delete={this.deleteNote} resize={this.resizeNote}/>
+          <BulletinBoard notes={this.state.notes} delete={this.deleteNote} resize={this.resizeNote} />
         </Layout>
       </ButtonContext.Provider>
     );
