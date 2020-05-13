fc784b54b9d92bec8b5c68c8c09a9056fd04dd69
GarvinH <garvinh2001@hotmail.com>
2020-05-13 10:04:02 -0400

    Updates

---
+++ precache-manifest.c8a42350408c67c57eeb52594902198e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "628df5324ff128079c58f19f938d2caa",
+    "url": "/StickyNotes/index.html"
+  },
+  {
+    "revision": "b6031bfda6c23dee1208",
+    "url": "/StickyNotes/static/css/main.74b5895f.chunk.css"
+  },
+  {
+    "revision": "a267523c00d419415ac3",
+    "url": "/StickyNotes/static/js/2.04d4bc36.chunk.js"
+  },
+  {
+    "revision": "e1ebcee444532ceab7d2a4a1c7007bba",
+    "url": "/StickyNotes/static/js/2.04d4bc36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6031bfda6c23dee1208",
+    "url": "/StickyNotes/static/js/main.154b0954.chunk.js"
+  },
+  {
+    "revision": "fe8c8ac82811a44c83d3",
+    "url": "/StickyNotes/static/js/runtime-main.647d1972.js"
+  },
+  {
+    "revision": "e7ee140a7d932283fd4979765609021d",
+    "url": "/StickyNotes/static/media/pallete.e7ee140a.png"
+  },
+  {
+    "revision": "2ecfff789a0b77dff64275ba143fc2af",
+    "url": "/StickyNotes/static/media/resize.2ecfff78.svg"
+  }
+]);
