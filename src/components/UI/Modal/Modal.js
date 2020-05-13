71c00a74622963cc942d2ae0e5ed9412d6b5e64e
GarvinH <garvinh2001@hotmail.com>
2020-05-13 14:40:45 -0400

    Minor optimizations to modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.module.css'
 import Backdrop from '../Backdrop/Backdrop'
 import Aux from '../../../hoc/Aux'
 import CloseButton from '../../UI/Button/CloseButton/CloseButton'
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} style={props.style} clicked={() => props.updateModal(0)} />
-        <div className={classes.modal} style={{ ...props.style, transform: (props.show === 0) ? "translateY(-100vh)" : "translateY(0)" }}>
-            <CloseButton clicked={() => props.updateModal(0)} />
-            {props.children}
-        </div>
-    </Aux>
-)
+class Modal extends Component {
 
-export default modal;
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} style={this.props.style} clicked={() => this.props.updateModal(0)} />
+                <div className={classes.modal} style={{ ...this.props.style, transform: (this.props.show === 0) ? "translateY(-100vh)" : "translateY(0)" }}>
+                    <CloseButton clicked={() => this.props.updateModal(0)} />
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+}
+
+export default Modal;
