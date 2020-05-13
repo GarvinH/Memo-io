0846fef51a685f4350d2d36c53d691447860414b
GarvinH <garvinh2001@hotmail.com>
2020-05-13 10:54:38 -0400

    Change logo icon

--- src/components/UI/Logo/Logo.js
+++ src/components/UI/Logo/Logo.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import logoImg from '../../../assets/logo.png'
+import logoImg from '../../../assets/memo.png'
 import classes from './Logo.module.css'
 
 const logo = (props) => (
