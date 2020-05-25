fbfb35b07c20f244f7d0c2e09c04fcee56484500
GarvinH <garvinh2001@hotmail.com>
2020-05-25 15:21:59 -0400

    Add missing proptypes to navitem.js

--- src/components/Navigation/NavBar/NavItems/NavItem/NavItem.js
+++ src/components/Navigation/NavBar/NavItems/NavItem/NavItem.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import classes from './NavItem.module.css'
+import PropTypes from 'prop-types'
 
 const navItem = (props) => (
     <li className={classes.NavItem} >
         <button aria-label={props.ariaLable? props.ariaLable : null} onClick={props.clicked}>{props.children}</button>
     </li>
 )
+
+navItem.propTypes = {
+    ariaLable: PropTypes.string,
+    clicked: PropTypes.func.isRequired
+}
 
 export default navItem;
