00810b369104d278639ce381916f8054f6427bc6
GarvinH <garvinh2001@hotmail.com>
2020-07-14 21:18:22 -0400

    Add posting

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
 const express = require('express')
 require('dotenv').config()
+const bodyParser = require('body-parser')
 const path = require('path')
 const mongoose = require('mongoose')
 const encrypt = require('mongoose-encryption')
+const passport = require('passport')
+const passportLocalMongoose = require('passport-local-mongoose')
+const session = require('express-session')
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+const port = process.env.PORT || 8080
+const app = express();
+const publicPath = path.join(__dirname, 'build')
+
+app.use(bodyParser.json())
+app.use(express.static(publicPath))
+app.use(session({
+    secret: process.env.COOKIE_ID,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    notes: [String]
+    notes: [{text: String, color: String}]
 })
 
-userSchema.plugin(encrypt, {secret: process.env.KEY, encryptedFields: ["notes"]})
+userSchema.plugin(passportLocalMongoose)
+userSchema.plugin(encrypt, {encryptionKey: process.env.ENC_KEY, signingKey: process.env.SIGN_KEY, encryptedFields: ["notes"]})
 const User = new mongoose.model("User", userSchema)
 
-const user = new User({email: "a@b.com", password: "test", notes: ["test", "string"]})
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+  
+
+const user = new User({email: "a@b.com", password: "test", notes: [{text: "test", color: "black"}, {text: "string", color:"red"}]})
 // user.save(function(err) {
 //     console.log(err)
 // })
@@ -27,13 +60,12 @@ const user = new User({email: "a@b.com", password: "test", notes: ["test", "stri
 //     }
 // })
 
-const port = process.env.PORT || 8080
-const app = express();
-const publicPath = path.join(__dirname, 'build')
-app.use(express.static(publicPath))
-
 app.get("/", function (req, res) {
     return res.sendFile(path.join(publicPath, "index.html"))
+})
+
+app.post("/register", function(req, res) {
+    console.log(req.body)
 })
 
 app.listen(port)
--- src/components/UI/Registration/Registration.js
+++ src/components/UI/Registration/Registration.js
@@ -1,37 +1,57 @@
-import React from 'react'
-import classes from './Registration.module.css'
-import Aux from '../../../hoc/Aux'
+import React from "react";
+import classes from "./Registration.module.css";
+import Aux from "../../../hoc/Aux";
+import axios from "axios";
+
+const register = (event) => {
+  event.preventDefault();
+  console.log(event.target.name.value);
+  axios.post("/register", {
+    name: event.target.name.value,
+    email: event.target.email.value,
+    password: event.target.password.value,
+    confirm: event.target.confirm.value,
+  });
+};
 
 const registsration = () => (
-    <Aux>
-        <h1 className={classes.title}>Sign Up</h1>
-        <hr></hr>
-        <form className={classes.signup}>
-            <label htmlFor="name">
-                Name:
-                <input type="text" placeholder="Your name here" />
-            </label>
+  <Aux>
+    <h1 className={classes.title}>Sign Up</h1>
+    <hr></hr>
+    <form onSubmit={(event) => register(event)}>
+      <div className={classes.signup}>
+        <label htmlFor="name">
+          Name:
+          <input type="text" placeholder="Your name here" name="name" />
+        </label>
 
-            <label htmlFor="email">
-                Email:
-                <input type="email" placeholder="Your email here" />
-            </label>
+        <label htmlFor="email">
+          Email:
+          <input type="email" placeholder="Your email here" name="email" />
+        </label>
+      </div>
+      <div className={classes.signup}>
+        <label htmlFor="password">
+          Password:
+          <input
+            type="password"
+            placeholder="Your password here"
+            name="password"
+          />
+        </label>
 
-        </form>
-        <form className={classes.signup}>
-            <label htmlFor="password">
-                Password:
-                <input type="password" placeholder="Your password here" />
-            </label>
-
-            <label htmlFor="confirm password">
-                Confirm Password:
-                <input type="password" placeholder="Confirm your password here" />
-            </label>
-
-        </form>
-        <input className={classes.submit} type="submit" value="Submit" />
-    </Aux>
-)
+        <label htmlFor="confirm">
+          Confirm Password:
+          <input
+            type="password"
+            placeholder="Confirm your password here"
+            name="confirm"
+          />
+        </label>
+      </div>
+      <input className={classes.submit} type="submit" value="Submit" />
+    </form>
+  </Aux>
+);
 
 export default registsration;
